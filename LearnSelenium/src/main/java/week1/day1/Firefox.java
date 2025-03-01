e02045d38148cf97d1f6ed696eda4fea756569d7
abinayaarthiraja <abinayaarthiraja@localhost>
2025-03-01 23:30:09 +0530

    test

---
+++ LearnSelenium/src/main/java/week1/day1/Firefox.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// type main do control+ space
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("Firefox details:"+ "\n"+ "BrowserVersion:"+browserVersion+ "\n" +"BrowserName:"+browserName+ "\n" +"isVisible:"
+		+isVisible+ "\n"+"releaseYear:"+releaseYear+"\n"+"browserLogo:"+browserLogo);
+	}
+
+}
